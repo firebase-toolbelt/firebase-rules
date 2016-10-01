b1025ab689fd285349b37ec9845a7327144a31ea
georgesboris <georgesboris@gmail.com>
2016-09-30 22:04:38 -0300

    changed: exports now defaults path to the path at which the process was started. this will usually mean the project root folder.

--- src/export.js
+++ src/export.js
@@ -4,14 +4,11 @@
  * 
  */
 
-const path = require('path');
 const jsonfile = require('jsonfile-promised');
 const { mergeRules } = require('./rules');
 
-jsonfile.spaces = 2;
-
-module.exports = function exportRules(rules, exportPath) {
-  if (!exportPath) { exportPath = path.join(__dirname, 'database.rules.json'); }
+module.exports = function exportRules(rules, path) {
+  if (!path) { path = process.cwd() + '/database.rules.json'; }
   if (rules.constructor === Array) { rules = mergeRules(rules); }
-  return jsonfile.writeFile(path, { rules });
+  return jsonfile.writeFile(path, { rules }, { spaces: 2 }).catch(console.log);
 }
