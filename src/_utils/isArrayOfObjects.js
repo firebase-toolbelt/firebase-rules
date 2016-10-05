1f3d3254be57949c920bde6939af6d13337d901c
georgesboris <georgesboris@gmail.com>
2016-10-04 22:27:24 -0300

    removed: unnecessary utils

--- src/_utils/isArrayOfObjects.js
+++
@@ -1,5 +0,0 @@
-const isObject = require('./isObject');
-
-module.exports = function isArrayOfObjects(value) {
-  return value && value.filter && value.filter((rule) => !isObject(rule)).length);
-};
--- src/_utils/isObject.js
+++
@@ -1,3 +0,0 @@
-module.exports = function _isObject(value) {
-  return value && value.constructor && value.constructor === Object;
-}
