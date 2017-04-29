ab4081e47d6fa33c67fbf71034b7177bd98e9de4
Georges Boris <georgesboris@gmail.com>
2017-04-28 22:38:45 -0300

    mergeConditions now accepts array as well as multiple arguments

--- src/_utils/mergeConditions.js
+++ src/_utils/mergeConditions.js
@@ -11,7 +11,8 @@
 module.exports = function mergeConditions(separator) {
   if (!separator) { throw new Error('firebase-rules: mergeConditions must receive a separator.'); }
   return function () {
-    const args = Array.from(arguments);
+    let args = Array.from(arguments);
+    if (args[0] && args[0].constructor === Array) { args = args[0]; }
     if (args.length < 1) { throw new Error('firebase-rules: mergeConditions must receive at least one condition.'); }
     return args.reduce((acc, condition, index) => {
       return (index < args.length - 1) ? acc + `(${condition})` + separator : acc + `(${condition})`;
