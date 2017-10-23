88a7e2f30229ebd56b63c6c8a31aefc80a93dc11
Georges Boris <georgesboris@gmail.com>
2017-10-23 15:11:40 -0300

    fix: typo when checking if filename was passed to index

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ const path = require('path');
 
 module.exports = function createRules(_rules, filepath) {
   const rules = buildRules(_rules);
-  return (path)
-    ? jsonfile.writeFile(path.resolve(process.cwd(), filepath), rules, { spaces: 2 }).catch(console.log)
+  return filepath
+    ? jsonfile
+        .writeFile(path.resolve(process.cwd(), filepath), rules, { spaces: 2 })
+        .catch(console.log)
     : rules;
-}
+};
